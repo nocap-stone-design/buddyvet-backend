97cb0b874877b8d9f190a4dac5cca521e6990ca0
jujuwon <webmon@kakao.com>
2023-01-23 20:23:56 +0900

    [BUD-9] feat: 프로젝트 생성

---
+++ src/main/java/com/capstone/buddyvet/BuddyvetApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.buddyvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/buddyvet/BuddyvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.buddyvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
