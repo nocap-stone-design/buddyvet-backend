731a6b9d1bcbf479c31331f8d935a6392ba1a9d7
jujuwon <webmon@kakao.com>
2023-02-16 15:04:10 +0900

    [BUD-30] fix: 로그인/회원가입 DTO 주석 추가 (#8)

--- src/main/java/com/capstone/buddyvet/dto/Auth.java
+++ src/main/java/com/capstone/buddyvet/dto/Auth.java
@@ -9,23 +9,32 @@ import lombok.Getter;
 @Getter
 public class Auth {
 
+	/**
+	 * 로그인/회원가입 Request
+	 */
 	@Getter
 	public static class LoginRequest {
 		private String accessToken;
 		private Provider providerType;
 	}
 
+	/**
+	 * 소셜 Provider 에서 받아온 유저 정보 DTO
+	 */
 	@Getter
 	@AllArgsConstructor
 	@Builder
-	public static class LoginResponse {
+	public static class SocialUserInfo {
 		private String loginId;
 		private Provider providerType;
 		private String nickname;
-		private String profileImage;
 	}
 
+	/**
+	 * 로그인/회원가입 jwt Response
+	 */
 	@Getter
+	@AllArgsConstructor
 	public static class JwtResponse {
 		private String jwt;
 	}
